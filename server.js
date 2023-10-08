6623abf7b1b126219bcab2045eb194a9a8a0f13a
season <sionwer4@naver.com>
2023-10-08 13:07:54 +0900

    feat: Add resolvers for tweet read

--- server.js
+++ server.js
@@ -2,21 +2,51 @@ import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    ping: String
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweet(){
+      return tweets;
+    }
+    tweet() {
+      console.log("Called.");
+      return null;
+    },
+    ping() {
+      return "pong";
+    },
+  },
+};
+
+const tweets = [
+  {
+    id: "1",
+    text: "First one",
+  },
+  {
+    id: "2",
+    text: "Second one",
+  },
+];
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running: ${url}`);
